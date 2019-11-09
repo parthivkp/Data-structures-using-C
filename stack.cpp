08a76b1b120be5da571a0bc1e4d7ce1b27903411
parthivkp <55494942+parthivkp@users.noreply.github.com>
2019-11-09 19:26:09 +0530

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,42 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#define max 30
+int ch,top=-1,s[max],iteam,i;
+ 
+int push(){
+	if (top==max-1){
+		printf("overflow\n");
+		
+	} else {
+		printf("enter the element to push\n");
+		scanf("%d",&iteam);
+		s[++top]=iteam;
+	}
+}  int pop(){
+	if (top==-1){
+		printf("undeflow\n");
+	} else{
+		printf("%d is deleted\n",s[top--]);
+	}
+} int display(){
+	if (top==-1){
+		printf("nothing to display\n");
+	}else {
+	
+	for(i=0;i<=top;i++)
+	{
+		printf("%d\n",s[i]);
+	}
+	}
+} int main(){ while(1){
+
+	printf("press\n 1 to push\n 2 to pop \n 3 to display \n 4.exit\n");
+	scanf("%d",&ch);
+	switch (ch){
+		case 1:push();break;
+		case 2:pop();break;
+		case 3:display();break;
+		case 4:exit(0);
+	}
+}}
